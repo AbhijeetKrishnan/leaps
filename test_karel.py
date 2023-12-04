28b817367bd604b4648426029ab4b74ddaaa2b5f
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2023-12-03 21:54:28 -0800

    Extract LEAPS reward function

--- test_karel.py
+++ test_karel.py
@@ -2,31 +2,48 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))) # hacky path manipulation to allow LEAPS code to be imported
 
+import torch
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))) # hacky path manipulation to allow LEAPS code to be imported
 from prl_gym.exec_env import ExecEnv2
+from rl.envs import make_vec_envs
 from pretrain.get_karel_config import get_karel_task_config
 
 
-if __name__ == '__main__':
-    
-    program_text = 'DEF run m( WHILE c( noMarkersPresent c) w( turnRight move w) putMarker move WHILE c( noMarkersPresent c) w( turnRight move w) putMarker move WHILE c( noMarkersPresent c) w( turnRight move w) turnRight move putMarker m)'
-    mdp_config = get_karel_task_config('topOff')
+def get_reward(program_text, seed, task):
+    mdp_config = get_karel_task_config(task)
+    mdp_config['seed'] = seed
+    mdp_config['args'].seed = seed
 
     program = program_text.replace('\\', '').replace('\'', '')
     
-    karel_env = ExecEnv2(mdp_config['args'])
-    reward, pred_program = karel_env.reward(program, is_program_str=True)
+    cfg_rl = mdp_config['rl']
+    cfg_envs = mdp_config['rl']['envs']
 
-    states_0 = pred_program['s_h'][0]
-    actions_0 = pred_program['a_h'][0]
-    rewards_0 = pred_program['reward_h'][0]
+    custom = True if "karel" or "CartPoleDiscrete" in cfg_envs['executable']['name'] else False
+    test_env = make_vec_envs(cfg_envs['executable']['name'], mdp_config['seed'], 1,
+                                cfg_rl['gamma'], '.', mdp_config['device'], False,
+                                custom_env=custom, custom_env_type='program', custom_kwargs={'config': mdp_config['args']})
+    test_env.reset()
 
-    actions_0_len = pred_program['a_h_len'][0]
+    program_seq = ExecEnv2(mdp_config['args']).dsl.str2intseq(program)[1:mdp_config['max_program_len']]
+    program_seq += [50] * (mdp_config['max_program_len'] - len(program_seq))
+    program_tensor = torch.tensor(program_seq, dtype=torch.int8, device=mdp_config['device']).unsqueeze(0)
+    _, reward, _, _ = test_env.step(program_tensor)
+    reward = reward.item()
 
-    for i in range(actions_0_len):
-        karel_env._world.print_state(states_0[i])
-        print(actions_0[i], rewards_0[i])
-    karel_env._world.print_state(states_0[actions_0_len])
+    return reward
+
+def main():
+    program_text = 'DEF run m( IFELSE c( frontIsClear c) i( move turnRight i) ELSE e( WHILE c( rightIsClear c) w( pickMarker w) e) IF c( frontIsClear c) i( move putMarker i) m)'
+    seed = 100
+    task = 'topOff'
+
+    reward = main(program_text, seed, task)
 
     print(reward)
+
+
+if __name__ == '__main__':
+    main()
